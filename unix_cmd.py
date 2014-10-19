e233286876d317891c1dab9a960d1d58f04ad40f
marcuswillock <marcus@adcade.com>
2014-10-18 21:08:22 -0400

    I have the basic command arg parse portion done

--- unix_cmd.py
+++ unix_cmd.py
@@ -1,3 +1,38 @@
 from __future__ import print_function
+import argparse
 
-print("Hello world")
+def cmd_parser():
+    """
+    The commandline parser logic
+    """
+
+    description = "A tool to help discover the terminal commands on your Unix Based Machine."
+    epilog = "use: 'man [cmd]' or '[cmd] -h' to see how that cmd is used"
+
+    # Initilize the arg parser
+    parser = argparse.ArgumentParser(description=description, epilog=epilog)
+
+    group1 = parser.add_mutually_exclusive_group(required=True)
+    group1.add_argument("-d, --default",
+          help="lists files from /usr/bin directory",
+          action="store_true")
+    group1.add_argument("-l, --local",
+          help="lists files from the /usr/local/bin directory",
+          action="store_true")
+    group1.add_argument("-a, --all",
+          help="Lists files from both /usr/bin and /usr/local/bin directories",
+          action="store_true")
+
+    group2 = parser.add_mutually_exclusive_group()
+    group2.add_argument("-s, --startswith",
+          help="applies a grep '^char' filter to the output",
+          type=str)
+    group2.add_argument("-i, --includes",
+          help="applies a grep 'string' filter to the output",
+          type=str)
+
+    args = parser.parse_args()
+    print(args)
+
+if __name__ == '__main__':
+  cmd_parser()
